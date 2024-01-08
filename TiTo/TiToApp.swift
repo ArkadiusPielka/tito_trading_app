a3a549d50115572a956f53bad5553693c00348e3
arkadiuspielka <akki1980@hotmail.de>
2024-01-08 09:07:43 +0100

    Start woche 2

---
+++ TiTo/TiToApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiToApp.swift
+//  TiTo
+//
+//  Created by Arkadius Pielka on 08.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TiToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
